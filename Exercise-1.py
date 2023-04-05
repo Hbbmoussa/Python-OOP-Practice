6cd2bddac08ac351816d8712b8fae985636e852e
Habibou Moussa <habibou997@gmail.com>
2023-04-05 13:58:12 +0000

    Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.

---
+++ Exercise-1.py
@@ -0,0 +1,7 @@
+class Vehicle():
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed,
+        self.mileage = mileage
+
+modelX = Vehicle(234, 10)
+print(modelX.max_speed,modelX.mileage)
