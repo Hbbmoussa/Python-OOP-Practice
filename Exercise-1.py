ef53207b5276b2fb3e1096fd116f1dbf6e9c648a
Habibou Moussa <moussa.habibou@meltwater.org>
2023-04-06 14:53:21 +0000

    Define a class attribute”color” with a default value white.

--- Exercise-1.py
+++ Exercise-1.py
@@ -1,15 +1,20 @@
 class Vehicle():
+    color = 'white'
     def __init__(self, name, max_speed, mileage):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
+        
 
-modelX = Vehicle("Tesla",234, 10)
+#modelX = Vehicle("Tesla",234, 10)
 #print(modelX.max_speed,modelX.mileage)
 
 class Bus(Vehicle):
     pass
 
-schoolBus = Bus('School bus',300,20 )
-print('Bus name:',schoolBus.name,',Speed:',schoolBus.max_speed,',mileage:',schoolBus.mileage)
+class Car(Vehicle):
+    pass
+
+car = Car('School bus',300,20 )
+print('Bus name:',car.name,',Speed:',car.max_speed,',mileage:',car.mileage, ',color:',car.color)
     
